6ce5c5a48c53ed05de7a1b7f7d6979e3cd0d61c9
licon4812 <jalickolli@gmail.com>
2023-12-01 18:26:01 +1100

    completed part 1 of puzzle 1

---
+++ 2023/AdventOfCode.2023.Puzzle1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+var data = File.ReadAllLines("../../../data.txt");
+var numbers = new List<string>();
+
+foreach (var line in data)
+{
+    var lineNumbers = line.Where(char.IsNumber).Aggregate("", (current, t) => current + t);
+    numbers.Add(lineNumbers);
+}
+
+var result = 0;
+foreach (var line in numbers)
+{
+   //combine the first and last number of each line
+   var first = line.First().ToString();
+   var last = line.Last().ToString();
+   var combined = first + last;
+   var combinedInt = int.Parse(combined);
+   result += combinedInt;
+}
+
+Console.WriteLine(result);
+
+
+
